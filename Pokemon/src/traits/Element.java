a333b1edf0d55a761ade35d917cc1a3653bf59f2
Nicholli Bernard <nmbernar@ncsu.edu>
2015-12-17 20:48:03 -0500

    Element methods completed, ready to transition from classes

--- Pokemon/src/traits/Element.java
+++ Pokemon/src/traits/Element.java
@@ -32,8 +32,7 @@ public enum Element {
 
 		@Override
 		public TypeSet getWeaknesses() {
-			return new TypeSet(new ArrayList<>(Arrays.asList(Element.ELECTRIC, 
-					Element.GRASS)));
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.ELECTRIC, Element.GRASS)));
 		}
 
 		@Override
@@ -43,8 +42,8 @@ public enum Element {
 
 		@Override
 		public TypeSet getCantAffect() {
-			// TODO Auto-generated method stub
-			return null;
+
+			return new TypeSet();
 		}
 
 	},
@@ -52,15 +51,13 @@ public enum Element {
 
 		@Override
 		public TypeSet getStrengths() {
-			return new TypeSet(new ArrayList<>(Arrays.asList(Element.ROCK, 
-					Element.GROUND, Element.WATER)));
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.ROCK, Element.GROUND, Element.WATER)));
 		}
 
 		@Override
 		public TypeSet getWeaknesses() {
-			return new TypeSet(new ArrayList<>(Arrays.asList(Element.BUG, 
-					Element.FIRE, Element.POISON, Element.FLYING, 
-					Element.ICE)));
+			return new TypeSet(new ArrayList<>(
+					Arrays.asList(Element.BUG, Element.FIRE, Element.POISON, Element.FLYING, Element.ICE)));
 		}
 
 		@Override
@@ -78,26 +75,23 @@ public enum Element {
 
 		@Override
 		public TypeSet getStrengths() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.WATER, Element.FLYING)));
 		}
 
 		@Override
 		public TypeSet getWeaknesses() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(Element.GROUND);
 		}
 
 		@Override
 		public TypeSet getUnaffected() {
-			// TODO Auto-generated method stub
-			return null;
+
+			return new TypeSet();
 		}
 
 		@Override
 		public TypeSet getCantAffect() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(Element.GROUND);
 		}
 
 	},
@@ -105,26 +99,23 @@ public enum Element {
 
 		@Override
 		public TypeSet getStrengths() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.GRASS, Element.FIGHTING, Element.BUG)));
 		}
 
 		@Override
 		public TypeSet getWeaknesses() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.ELECTRIC, Element.ICE, Element.ROCK)));
 		}
 
 		@Override
 		public TypeSet getUnaffected() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(Element.GROUND);
 		}
 
 		@Override
 		public TypeSet getCantAffect() {
-			// TODO Auto-generated method stub
-			return null;
+
+			return new TypeSet();
 		}
 
 	},
@@ -132,26 +123,25 @@ public enum Element {
 
 		@Override
 		public TypeSet getStrengths() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.FIRE, Element.ICE, Element.FLYING, Element.BUG)));
 		}
 
 		@Override
 		public TypeSet getWeaknesses() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(new ArrayList<>(
+					Arrays.asList(Element.WATER, Element.GRASS, Element.FIGHTING, Element.STEEL, Element.GROUND)));
 		}
 
 		@Override
 		public TypeSet getUnaffected() {
-			// TODO Auto-generated method stub
-			return null;
+
+			return new TypeSet();
 		}
 
 		@Override
 		public TypeSet getCantAffect() {
-			// TODO Auto-generated method stub
-			return null;
+
+			return new TypeSet();
 		}
 
 	},
@@ -159,20 +149,17 @@ public enum Element {
 
 		@Override
 		public TypeSet getStrengths() {
-			return new TypeSet(new ArrayList<>(Arrays.asList(Element.PSYCHIC, 
-					Element.GHOST)));
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.PSYCHIC, Element.GHOST)));
 		}
 
 		@Override
 		public TypeSet getWeaknesses() {
-			return new TypeSet(new ArrayList<>(Arrays.asList(Element.GHOST, 
-					Element.DARK)));
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.GHOST, Element.DARK)));
 		}
 
 		@Override
 		public TypeSet getUnaffected() {
-			return new TypeSet(new ArrayList<>(Arrays.asList(Element.NORMAL, 
-				Element.FIGHTING)));
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.NORMAL, Element.FIGHTING)));
 		}
 
 		@Override
@@ -185,26 +172,24 @@ public enum Element {
 
 		@Override
 		public TypeSet getStrengths() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.GRASS, Element.PSYCHIC, Element.DARK)));
 		}
 
 		@Override
 		public TypeSet getWeaknesses() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.FIRE, Element.FLYING, Element.ROCK)));
 		}
 
 		@Override
 		public TypeSet getUnaffected() {
-			// TODO Auto-generated method stub
-			return null;
+
+			return new TypeSet();
 		}
 
 		@Override
 		public TypeSet getCantAffect() {
-			// TODO Auto-generated method stub
-			return null;
+
+			return new TypeSet();
 		}
 
 	},
@@ -212,26 +197,24 @@ public enum Element {
 
 		@Override
 		public TypeSet getStrengths() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet();
 		}
 
 		@Override
 		public TypeSet getWeaknesses() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(Element.FIGHTING);
 		}
 
 		@Override
 		public TypeSet getUnaffected() {
-			// TODO Auto-generated method stub
-			return null;
+
+			return new TypeSet();
 		}
 
 		@Override
 		public TypeSet getCantAffect() {
-			// TODO Auto-generated method stub
-			return null;
+
+			return new TypeSet();
 		}
 
 	},
@@ -239,26 +222,24 @@ public enum Element {
 
 		@Override
 		public TypeSet getStrengths() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.ICE, Element.ROCK, Element.FAIRY)));
 		}
 
 		@Override
 		public TypeSet getWeaknesses() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.FIGHTING, Element.FIRE, Element.GROUND)));
 		}
 
 		@Override
 		public TypeSet getUnaffected() {
-			// TODO Auto-generated method stub
-			return null;
+
+			return new TypeSet(Element.POISON);
 		}
 
 		@Override
 		public TypeSet getCantAffect() {
-			// TODO Auto-generated method stub
-			return null;
+
+			return new TypeSet();
 		}
 
 	},
@@ -266,26 +247,24 @@ public enum Element {
 
 		@Override
 		public TypeSet getStrengths() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(new ArrayList<>(
+					Arrays.asList(Element.NORMAL, Element.ICE, Element.ROCK, Element.DARK, Element.STEEL)));
 		}
 
 		@Override
 		public TypeSet getWeaknesses() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.FLYING, Element.PSYCHIC, Element.FAIRY)));
 		}
 
 		@Override
 		public TypeSet getUnaffected() {
-			// TODO Auto-generated method stub
-			return null;
+
+			return new TypeSet();
 		}
 
 		@Override
 		public TypeSet getCantAffect() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(Element.GHOST);
 		}
 
 	},
@@ -298,8 +277,7 @@ public enum Element {
 
 		@Override
 		public TypeSet getWeaknesses() {
-			return new TypeSet(Arrays.asList(Element.ICE, 
-					Element.DRAGON, Element.FAIRY));
+			return new TypeSet(Arrays.asList(Element.ICE, Element.DRAGON, Element.FAIRY));
 		}
 
 		@Override
@@ -317,26 +295,23 @@ public enum Element {
 
 		@Override
 		public TypeSet getStrengths() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.FIGHTING, Element.DRAGON, Element.DARK)));
 		}
 
 		@Override
 		public TypeSet getWeaknesses() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.POISON, Element.STEEL)));
 		}
 
 		@Override
 		public TypeSet getUnaffected() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(Element.DRAGON);
 		}
 
 		@Override
 		public TypeSet getCantAffect() {
-			// TODO Auto-generated method stub
-			return null;
+
+			return new TypeSet();
 		}
 
 	},
@@ -344,14 +319,12 @@ public enum Element {
 
 		@Override
 		public TypeSet getStrengths() {
-			return new TypeSet(new ArrayList<>(Arrays.asList(Element.PSYCHIC, 
-					Element.GHOST)));
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.PSYCHIC, Element.GHOST)));
 		}
 
 		@Override
 		public TypeSet getWeaknesses() {
-			return new TypeSet(new ArrayList<>(Arrays.asList(Element.FIGHTING, 
-					Element.BUG, Element.FAIRY)));
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.FIGHTING, Element.BUG, Element.FAIRY)));
 		}
 
 		@Override
@@ -369,26 +342,26 @@ public enum Element {
 
 		@Override
 		public TypeSet getStrengths() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(
+					new ArrayList<>(Arrays.asList(Element.GRASS, Element.GROUND, Element.FLYING, Element.DRAGON)));
 		}
 
 		@Override
 		public TypeSet getWeaknesses() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(
+					new ArrayList<>(Arrays.asList(Element.FIRE, Element.FIGHTING, Element.ROCK, Element.STEEL)));
 		}
 
 		@Override
 		public TypeSet getUnaffected() {
-			// TODO Auto-generated method stub
-			return null;
+
+			return new TypeSet();
 		}
 
 		@Override
 		public TypeSet getCantAffect() {
-			// TODO Auto-generated method stub
-			return null;
+
+			return new TypeSet();
 		}
 
 	},
@@ -396,26 +369,23 @@ public enum Element {
 
 		@Override
 		public TypeSet getStrengths() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(new ArrayList<>(
+					Arrays.asList(Element.FIRE, Element.ELECTRIC, Element.POISON, Element.ROCK, Element.STEEL)));
 		}
 
 		@Override
 		public TypeSet getWeaknesses() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.WATER, Element.GRASS, Element.ICE)));
 		}
 
 		@Override
 		public TypeSet getUnaffected() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(Element.ELECTRIC);
 		}
 
 		@Override
 		public TypeSet getCantAffect() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(Element.FLYING);
 		}
 
 	},
@@ -423,14 +393,12 @@ public enum Element {
 
 		@Override
 		public TypeSet getStrengths() {
-			return new TypeSet(new ArrayList<>(Arrays.asList(Element.FIGHTING, 
-					Element.POISON)));
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.FIGHTING, Element.POISON)));
 		}
 
 		@Override
 		public TypeSet getWeaknesses() {
-			return new TypeSet(new ArrayList<>(Arrays.asList(Element.BUG, 
-					Element.GHOST, Element.DARK)));
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.BUG, Element.GHOST, Element.DARK)));
 		}
 
 		@Override
@@ -448,38 +416,37 @@ public enum Element {
 
 		@Override
 		public TypeSet getStrengths() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.GRASS, Element.FAIRY)));
 		}
 
 		@Override
 		public TypeSet getWeaknesses() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(new ArrayList<>(Arrays.asList(Element.GROUND, Element.PSYCHIC)));
 		}
 
 		@Override
 		public TypeSet getUnaffected() {
-			// TODO Auto-generated method stub
-			return null;
+
+			return new TypeSet();
 		}
 
 		@Override
 		public TypeSet getCantAffect() {
-			// TODO Auto-generated method stub
-			return null;
+			return new TypeSet(Element.STEEL);
 		}
 
 	};
 
 	/**
 	 * Gets the elements that this element is super-effective against
+	 * 
 	 * @return TypeSet set of types
 	 */
 	public abstract TypeSet getStrengths();
 
 	/**
 	 * Gets the elements that are super-effective against this element
+	 * 
 	 * @return TypeSet set of types
 	 * @return
 	 */
@@ -487,12 +454,14 @@ public enum Element {
 
 	/**
 	 * Gets the elements that are unaffected by this element
+	 * 
 	 * @return TypeSet set of types
 	 */
 	public abstract TypeSet getUnaffected();
 
 	/**
 	 * Gets the elements that this element can't affect
+	 * 
 	 * @return TypeSet set of types
 	 */
 	public abstract TypeSet getCantAffect();
